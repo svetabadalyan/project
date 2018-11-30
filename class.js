7dbd584eb6fad8ce689f2f5dc7a7f706c38ffe8c
yourusername <your@email.com>
2018-11-30 21:08:42 +0400

    Project

---
+++ class.js
@@ -0,0 +1,455 @@
+class Grass {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+    mult() {
+        var empty = random(this.chooseCell(0));
+        this.multiply++;
+
+        if (empty && this.multiply > 3) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 1;
+            var newGr = new Grass(newX, newY);
+            grassArr.push(newGr);
+            this.multiply = 0;
+        }
+    }
+}
+
+class Xotaker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+    mult() {
+        var empty = random(this.chooseCell(0));
+
+        if (empty && this.energy > 5) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 2;
+            var xt = new Xotaker(newX, newY);
+            xotakerArr.push(xt);
+            this.multiply = 0;
+        }
+    }
+    move() {
+        var empty = random(this.chooseCell(0));
+        this.energy--;
+        if (empty) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 2;
+            matrix[this.y][this.x] = 0;
+
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+
+    eat() {
+        var food = random(this.chooseCell(1));
+        if (food) {
+            var newX = food[0];
+            var newY = food[1];
+            matrix[newY][newX] = 2;
+            matrix[this.y][this.x] = 0;
+
+            for (var i in grassArr) {
+                if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                    grassArr.splice(i, 1);
+                }
+            }
+
+            this.x = newX;
+            this.y = newY;
+            this.energy += 2;
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in xotakerArr) {
+                if (xotakerArr[i].x == this.x && xotakerArr[i].y == this.y) {
+                    xotakerArr.splice(i, 1);
+                }
+            }
+        }
+    }
+}
+
+
+class Gishatich {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+    mult() {
+        var empty = random(this.chooseCell(0));
+
+        if (empty && this.energy > 10) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 2;
+            var xt = new Gishatich(newX, newY);
+            gishatichArr.push(xt);
+            this.multiply = 0;
+        }
+    }
+    move() {
+        var empty = random(this.chooseCell(0));
+        this.energy--;
+        if (empty) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 3;
+            matrix[this.y][this.x] = 0;
+
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+
+    eat() {
+        var food = random(this.chooseCell(2));
+        if (food) {
+            var newX = food[0];
+            var newY = food[1];
+            matrix[newY][newX] = 3;
+            matrix[this.y][this.x] = 0;
+
+            for (var i in xotakerArr) {
+                if (xotakerArr[i].x == newX && xotakerArr[i].y == newY) {
+                    xotakerArr.splice(i, 1);
+                }
+            }
+
+            this.x = newX;
+            this.y = newY;
+            this.energy += 2;
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in gishatichArr) {
+                if (gishatichArr[i].x == this.x && gishatichArr[i].y == this.y) {
+                    gishatichArr.splice(i, 1);
+                }
+            }
+        }
+    }
+}
+
+class Amenaker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 5;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+    mult() {
+        var empty = random(this.chooseCell(0));
+
+        if (empty && this.energy > 10) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 2;
+            var xt = new Amenaker(newX, newY);
+            amenakerArr.push(xt);
+            this.multiply = 0;
+        }
+    }
+    move() {
+        var empty = random(this.chooseCell(0));
+        this.energy--;
+        if (empty) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 4;
+            matrix[this.y][this.x] = 0;
+
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+
+    eatXotaker() {
+        var food = random(this.chooseCell(2));
+        if (food) {
+            var newX = food[0];
+            var newY = food[1];
+            matrix[newY][newX] = 4;
+            matrix[this.y][this.x] = 0;
+
+            for (var i in xotakerArr) {
+                if (xotakerArr[i].x == newX && xotakerArr[i].y == newY) {
+                    xotakerArr.splice(i, 1);
+                }
+            }
+
+            this.x = newX;
+            this.y = newY;
+            this.energy += 2;
+        }
+    }
+    eatGishatich() {
+        var food = random(this.chooseCell(3));
+        if (food) {
+            var newX = food[0];
+            var newY = food[1];
+            matrix[newY][newX] = 4;
+            matrix[this.y][this.x] = 0;
+
+            for (var i in gishatichArr) {
+                if (gishatichArr[i].x == newX && gishatichArr[i].y == newY) {
+                    gishatichArr.splice(i, 1);
+                }
+            }
+
+            this.x = newX;
+            this.y = newY;
+            this.energy += 2;
+        }
+    }
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in amenakerArr) {
+                if (amenakerArr[i].x == this.x && amenakerArr[i].y == this.y) {
+                    amenakerArr.splice(i, 1);
+                }
+            }
+        }
+    }
+}
+
+
+class Wizard {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 5;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewDirections();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+
+    move() {
+        var empty = random(this.chooseCell(0));
+        if (empty) {
+            var newX = empty[0];
+            var newY = empty[1];
+            matrix[newY][newX] = 5;
+            matrix[this.y][this.x] = 0;
+
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+
+    Create() {
+        var stextsvox = random(this.chooseCell(0));
+        if (stextsvox) {
+            var newX = stextsvox[0];
+            var newY = stextsvox[1];
+            matrix[newY][newX] = Math.round(Math.random() * 3 + 1);
+            
+            if (matrix[newY][newX] == 1) {
+                var gr = new Grass(newX,newY)
+                grassArr.push(gr);
+                
+            }
+            else if (matrix[newY][newX] == 2) {
+                var xk = new Xotaker(newX,newY)
+                xotakerArr.push(xk);
+            }
+            else if (matrix[newY][newX] == 3) {
+                var gish = new Gishatich(newX,newY)
+                gishatichArr.push(gish);
+            }
+            else if (matrix[newY][newX] == 4) {
+                var ak = new Amenaker(newX,newY)
+                amenakerArr.push(ak);
+            }
+
+        }
+    }
+}
+
+
---
+++ script.js
@@ -0,0 +1,107 @@
+var matrix = [];
+
+for (i = 0; i < 40; i++) {
+    matrix[i] = [];
+    for (a = 0; a < 40; a++) {
+        matrix[i][a] = Math.round(Math.random() * 5)
+    }
+}
+
+var side = 20;
+var grassArr = [];
+var xotakerArr = [];
+var gishatichArr = [];
+var amenakerArr = [];
+var wizardArr = [];
+
+function setup() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var xt = new Xotaker(x, y);
+                xotakerArr.push(xt);
+            }
+            else if (matrix[y][x] == 3) {
+                var gish = new Gishatich(x, y);
+                gishatichArr.push(gish);
+            }
+            else if (matrix[y][x] == 4) {
+                var amenaker = new Amenaker(x, y);
+                amenakerArr.push(amenaker);
+            }
+            else if (matrix[y][x] == 5) {
+                var wizard = new Wizard(x, y);
+                wizardArr.push(wizard);
+            }
+
+
+        }
+    }
+
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+}
+
+function draw() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                fill("green");
+            }
+            else if (matrix[y][x] == 2) {
+                fill('yellow');
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 3) {
+                fill('blue');
+            }
+            else if (matrix[y][x] == 4) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("#f600ff");
+            }
+
+            rect(x * side, y * side, side, side)
+
+            fill("blue")
+        }
+    }
+
+    for (var i in grassArr) {
+        grassArr[i].mult();
+    }
+    for (var p in xotakerArr) {
+        xotakerArr[p].eat();
+        xotakerArr[p].move();
+        xotakerArr[p].mult();
+        xotakerArr[p].die();
+    }
+    for (var p in gishatichArr) {
+        gishatichArr[p].eat();
+        gishatichArr[p].move();
+        gishatichArr[p].mult();
+        gishatichArr[p].die();
+    }
+    for (var p in amenakerArr) {
+        amenakerArr[p].eatXotaker();
+        amenakerArr[p].eatGishatich();
+        amenakerArr[p].move();
+        amenakerArr[p].mult();
+        amenakerArr[p].die();
+    }
+    for (var p in wizardArr) {
+        wizardArr[p].Create();
+        wizardArr[p].move();
+
+    }
+
+}
+
